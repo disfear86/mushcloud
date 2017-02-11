b5cd18b922a8ef35efe804a64a8c1615efbcda4e
disfear <disfear@192.168.1.6>
2017-02-11 20:27:36 +0200

    Updated __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
+from flask_login import LoginManager
 
 app = Flask(__name__)
-
 app.config.from_object('config')
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 db = SQLAlchemy(app)
 mail = Mail(app)
+
+login_manager.login_view = 'login_page'
+
 
 from . import views
